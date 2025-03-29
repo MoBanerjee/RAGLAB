b426b7dec6a134db5f1368b5f97194d9e4f420a9
#BANERJEE MOHOR# <mohor001@asp2a-login-ntu01.head.cm.asp2a.nscc.sg>
2025-03-29 10:26:01 +0800

    raglab changes

--- model_down.sh
+++ model_down.sh
@@ -8,7 +8,7 @@
 #PBS -N mo
 module load miniforge3
 conda activate raglab
-huggingface-cli login --token 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
+
 #huggingface-cli download neuralmagic/Meta-Llama-3.1-8B-Instruct-quantized.w8a16 --local-dir RAGLAB/model/generator/Llama3-8B-baseline
 #huggingface-cli download meta-llama/Llama-3.1-8B-Instruct --local-dir modellama/lama
 huggingface-cli download colbert-ir/colbertv2.0 --local-dir RAGLAB/model/retriever/colbertv2.0
